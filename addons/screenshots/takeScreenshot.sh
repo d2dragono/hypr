ba9926f888f43805067cbeb654e18a95c6886567
d2dragono <dragono.subservice@pm.me>
2024-10-05 16:54:22 +0200

    Cleaned and improved every scripts. However, broke lots of stuff. Will need fixing

--- addons/screenshots/takeScreenshot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-grim -g "$(slurp)" - | wl-copy
-dunstify "Screenshot of the region taken" -t 3000 -r 21 --icon $hyprPath/addons/screenshots/snapshot.icon
-wl-paste | swappy -f - -o ~/Pictures/Screenshots/Screenshot-$(date +%F_%T | sed "s/://g" | sed "s/-//g")
-dunstify "Screenshot saved or copied to clipboard successfully." -t 3000 -r 21 --icon $hyprPath/addons/screenshots/saved.icon
-
-
--- addons/workspaces/scripts/pluggedMonitorList.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo `hyprctl monitors | grep 'Monitor' | sed "s/Monitor //g" | sed "s/ (ID /:/g" | sed "s/)://g"`
